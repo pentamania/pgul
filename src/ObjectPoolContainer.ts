d5a9cfdf1b8535afe8de73226291b3e139cb3b4b
pentamania <tanigami7220@gmail.com>
2021-01-08 11:58:23 +0900

    Add ObjectPoolContainer.fill
    Add pool key Generics too

--- src/ObjectPoolContainer.ts
+++ src/ObjectPoolContainer.ts
@@ -4,13 +4,13 @@ type ObjectResetter = (obj: any) => void;
 /**
  * @class
  */
-export class ObjectPoolContainer<T = any> {
-  private _pools: Map<string, T[]> = new Map();
+export class ObjectPoolContainer<KT = string, T = any> {
+  private _pools: Map<KT, T[]> = new Map();
   private _objReadyChecker: ReadyChecker = (obj: any | undefined) =>
     obj ? obj.parent == null : false;
   private _objectResetter?: ObjectResetter;
 
-  setPool(key: string) {
+  setPool(key: KT) {
     if (this._pools.has(key)) return this;
 
     const pool: T[] = [];
@@ -18,14 +18,14 @@ export class ObjectPoolContainer<T = any> {
     return pool;
   }
 
-  getPool(key: string) {
+  getPool(key: KT) {
     return this._pools.get(key);
   }
 
   /**
    * プーリングする
    */
-  pool(key: string, obj: T) {
+  pool(key: KT, obj: T) {
     if (!this._pools.has(key)) this.setPool(key);
     const pool = this._pools.get(key);
     if (!pool) {
@@ -35,8 +35,28 @@ export class ObjectPoolContainer<T = any> {
     pool.push(obj);
   }
 
-  // fill(key, $class, args:[], num) {
-  // }
+  /**
+   * オブジェクトを指定回数分取得、プール追加する
+   * @param poolKey
+   * @param objectFillNum
+   * @param objectGetFunc
+   */
+  fill(
+    poolKey: KT,
+    objectFillNum: number,
+    objectGetFunc: (i?: number, n?: number) => T
+  ) {
+    if (!this._pools.has(poolKey)) this.setPool(poolKey);
+    const pool = this._pools.get(poolKey)!;
+    for (let i = 0; i < objectFillNum; i++) {
+      const item = objectGetFunc(i);
+      if (!item) {
+        // Callback should return something
+        return;
+      }
+      pool.push(item);
+    }
+  }
 
   /**
    * 同期的にプールからオブジェクトを取り出す
