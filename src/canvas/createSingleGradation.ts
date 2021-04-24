2c47b86ef351853dc9b9e6a00edc6222c7c5282b
pentamania <tanigami7220@gmail.com>
2021-04-24 15:46:06 +0900

    [add] Add canvasHelper: createHorizontalMonoGradation
    Temporary

--- src/canvas/createSingleGradation.ts
+++ src/canvas/createSingleGradation.ts
@@ -1,6 +1,26 @@
 import { SharedContext } from "./common";
 
 /**
+ * @param range
+ * @param color
+ */
+export function createHorizontalMonoGradation(
+  width: number,
+  height: number,
+  color: string,
+  fromLeft: boolean = true
+) {
+  const ctx = SharedContext;
+  const y = height / 2;
+  const grad = ctx.createLinearGradient(0, y, width, y);
+  const c0 = fromLeft ? color : "transparent";
+  const c1 = c0 === "transparent" ? color : "transparent";
+  grad.addColorStop(0, c0);
+  grad.addColorStop(1, c1);
+  return grad;
+}
+
+/**
  * 単色のシンプルな円形グラデーションを作成
  * @param radius 半径
  * @param color 色
