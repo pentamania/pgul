0a120e72b0e477dc5664c9b731311b1566d6f0b9
pentamania <tanigami7220@gmail.com>
2021-05-26 16:29:25 +0900

    [add] Add utils/radianConverter.toRadianFromIntTable

--- src/utils/radianConverter.ts
+++ src/utils/radianConverter.ts
@@ -1,3 +1,5 @@
+import timesMap from "./timesMap";
+
 const DEG_TO_RAD = Math.PI / 180;
 const RAD_TO_DEG = 180 / Math.PI;
 
@@ -7,4 +9,22 @@ export function toRadian(deg: number) {
 
 export function toDegree(rad: number) {
   return rad * RAD_TO_DEG;
+}
+
+export const DEG_TO_RAD_TABLE = (() => {
+  return timesMap(360, (i) => {
+    return toRadian(i);
+  });
+})();
+
+/**
+ * 整数値テーブルを使ってラジアン変換する
+ *
+ * @param deg 整数
+ * @returns 整数以外はundefinedを返す
+ */
+export function toRadianFromIntTable(deg: number): number | undefined {
+  if (360 < deg) deg %= 360;
+  if (deg < 0) deg += 360;
+  return DEG_TO_RAD_TABLE[deg];
 }
