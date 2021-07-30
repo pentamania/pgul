66c0f13ab983ae3cb1e458ca336e4ac39afb5129
pentamania <tanigami7220@gmail.com>
2021-07-30 15:59:56 +0900

    [add] Add tests for utils/radianConverter
    Test only for flipRadianVertical

---
+++ test/utils/radianConverter.spec.ts
@@ -0,0 +1,67 @@
+import { flipRadianVertical, toRadian } from "../../src/utils/radianConverter";
+
+describe("radianConverter#flipRadianVertical", () => {
+  const toleratedFloatDigits = 5;
+
+  test("Basic: deg 0 be deg 180", () => {
+    expect(flipRadianVertical(0)).toBeCloseTo(
+      toRadian(180),
+      toleratedFloatDigits
+    );
+  });
+  test("Basic: deg 45 be deg 135", () => {
+    expect(flipRadianVertical(toRadian(45))).toBeCloseTo(
+      toRadian(135),
+      toleratedFloatDigits
+    );
+  });
+  test("Boundary: deg 90 would not change", () => {
+    const val = toRadian(90);
+    expect(flipRadianVertical(val)).toBeCloseTo(val, toleratedFloatDigits);
+  });
+  test("Basic: deg 180 be 0", () => {
+    expect(flipRadianVertical(toRadian(180))).toBeCloseTo(
+      0,
+      toleratedFloatDigits
+    );
+  });
+  test("Basic: deg 225 be deg 315", () => {
+    expect(flipRadianVertical(toRadian(225))).toBeCloseTo(
+      toRadian(315),
+      toleratedFloatDigits
+    );
+  });
+
+  test("Boundary: deg 270 would not change", () => {
+    const val = toRadian(270);
+    expect(flipRadianVertical(val)).toBeCloseTo(val, toleratedFloatDigits);
+  });
+
+  test("Basic: deg 315 be deg 225", () => {
+    expect(flipRadianVertical(toRadian(315))).toBeCloseTo(
+      toRadian(225),
+      toleratedFloatDigits
+    );
+  });
+
+  test("Should be normalized: deg 405(45) be deg 135", () => {
+    expect(flipRadianVertical(toRadian(405))).toBeCloseTo(
+      toRadian(135),
+      toleratedFloatDigits
+    );
+  });
+
+  // Positize
+  test("Positizing test: deg -45 to be deg 225", () => {
+    expect(flipRadianVertical(toRadian(-45), true)).toBeCloseTo(
+      toRadian(225),
+      toleratedFloatDigits
+    );
+  });
+  test("No Positizing test: deg -45 to be deg -135", () => {
+    expect(flipRadianVertical(toRadian(-45), false)).toBeCloseTo(
+      toRadian(-135),
+      toleratedFloatDigits
+    );
+  });
+});
