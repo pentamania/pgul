b7a2da34b50c5c2dfdca54ea47c91362e7e79ad4
pentamania <tanigami7220@gmail.com>
2021-08-09 16:07:57 +0900

    [add] Add utils types
    LooseFunction, EventListenable

--- src/utilTypes.ts
+++ src/utilTypes.ts
@@ -5,6 +5,8 @@ export type LooseObject = Record<string | number, any>;
 
 export type LooseVector2 = { x: number; y: number; [key: string]: any };
 
+export type LooseFunction = (...args: any) => any;
+
 /**
  * コンテキストを設定可能なGeneratorFunctionもどきの型
  */
@@ -14,3 +16,8 @@ export type ContextBindableGeneratorFunction<CTX = any> = (
 ) => Generator;
 
 export type Bit = 0 | 1;
+
+export interface EventListenable {
+  on: (eventType: string, handler: LooseFunction) => any;
+  off: (eventType: string, handler: LooseFunction) => any;
+}
