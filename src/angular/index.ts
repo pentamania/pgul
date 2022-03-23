aa7d4033246b45c6895736dd758b384d2c796afa
pentamania <tanigami7220@gmail.com>
2022-03-23 21:29:35 +0900

    [mod] Move mixins/Revolutional to 'angular' dir

--- src/angular/index.ts
+++ src/angular/index.ts
@@ -1,1 +1,2 @@
 export * from "./Waveformer";
+export * from "./Revolutional";
--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@ export * from "./gui/index";
 
 export * from "./mixins/Menuable";
 export * from "./mixins/Traceable";
-export * from "./mixins/Revolutional";
 export * from "./mixins/Poolable";
 export * from "./mixins/Chargable";
 export * from "./mixins/Vector2Driven";
