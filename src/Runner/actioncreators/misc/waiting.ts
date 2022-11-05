44e2fd1f4e5f4cfd76317f0d02d21168d8319d9e
pentamania <tanigami7220@gmail.com>
2022-11-05 16:24:47 +0900

    [add] Runner/actioncreators: Add createRunnerEndWaitingAction

--- src/Runner/actioncreators/misc/waiting.ts
+++ src/Runner/actioncreators/misc/waiting.ts
@@ -1,4 +1,4 @@
-import { RunnerAction } from "../../Runner";
+import { Runner, RunnerAction } from "../../Runner";
 
 /**
  * 何もしない
@@ -10,5 +10,15 @@ export function createWaitingAction(duration: number = Infinity): RunnerAction {
     while (_currentCount < duration) {
       yield _currentCount++;
     }
+  };
+}
+
+/**
+ * 指定したRunnerの活動停止を待つ
+ * @param runner
+ */
+export function createRunnerEndWaitingAction(runner: Runner): RunnerAction {
+  return function* () {
+    while (!runner.dead) yield;
   };
 }
