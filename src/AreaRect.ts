53f322aadee27dbe64d00022f2a9e6f1fe796b1d
pentamania <tanigami7220@gmail.com>
2022-11-17 12:57:02 +0900

    [mod] AreaRect: Remove margin feature
    For simplicity: Remove margin param, outOfOuterRect, elementOutofOuterRect, and outerXxx getters

--- src/AreaRect.ts
+++ src/AreaRect.ts
@@ -7,20 +7,12 @@ export class AreaRect {
   private _left: number;
   private _right: number;
   private _bottom: number;
-  private _margin: number;
 
-  constructor(
-    left: number,
-    top: number,
-    right: number,
-    bottom: number,
-    margin = 0
-  ) {
+  constructor(left: number, top: number, right: number, bottom: number) {
     this._left = left;
     this._top = top;
     this._right = right;
     this._bottom = bottom;
-    this._margin = margin;
   }
 
   /**
@@ -59,33 +51,6 @@ export class AreaRect {
   elementWithinBound(element: LooseVector2): boolean {
     return this.withinBound(element.x, element.y);
   }
-
-  /**
-   * マージン範囲を含めた矩形の外かどうか
-   * @param x
-   * @param y
-   */
-  outOfOuterRect(x: number, y: number): boolean {
-    return (
-      x < this.outerLeft ||
-      this.outerRight < x ||
-      y < this.outerTop ||
-      this.outerBottom < y
-    );
-  }
-
-  elementOutofOuterRect(element: LooseVector2) {
-    return this.outOfOuterRect(element.x, element.y);
-  }
-
-  // outOfInnerRect(x: number, y: number): boolean {
-  //   return (
-  //     x < this.outerLeft ||
-  //     this.outerRight < x ||
-  //     y < this.outerTop ||
-  //     this.outerBottom < y
-  //   );
-  // }
 
   ratioX(n: number) {
     return this._left + this.width * n;
@@ -154,18 +119,5 @@ export class AreaRect {
   }
   get centerY() {
     return this.height / 2;
-  }
-
-  get outerLeft() {
-    return this._left - this._margin;
-  }
-  get outerTop() {
-    return this._top - this._margin;
-  }
-  get outerRight() {
-    return this._right + this._margin;
-  }
-  get outerBottom() {
-    return this._bottom + this._margin;
   }
 }
