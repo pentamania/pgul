3dbf21251a23efd5cfc11ce2830dd02a85cef276
pentamania <tanigami7220@gmail.com>
2023-06-24 10:26:09 +0900

    [test] Update gui/MenuController test
    Refactoring, add new case, etc.

--- test/gui/MenuController.spec.ts
+++ test/gui/MenuController.spec.ts
@@ -1,21 +1,28 @@
-import { MenuController } from "../../src/gui/menu/MenuController";
+import {
+  MenuController,
+  MenuRequiredProps,
+} from "../../src/gui/menu/MenuController";
 import { Menuify } from "../../src/gui/menu/Menuify";
 import { FocusableMenuItem } from "../../src/gui/menu/MenuItem";
 
-// Item
-class MainMenuItem implements FocusableMenuItem {
+// Item class
+class MenuItem implements FocusableMenuItem {
   id: number = 0;
   alpha = 0.5;
 
   focus() {
-    this.alpha = MainMenuItem.alphaValWhenActive;
+    this.alpha = MenuItem.alphaValWhenActive;
   }
   defocus() {
     this.alpha = 0.5;
   }
+
+  /** Helper method to check that the item is selected */
   isActive(): boolean {
-    return this.alpha === MainMenuItem.alphaValWhenActive;
+    return this.alpha === MenuItem.alphaValWhenActive;
   }
+
+  /** @virtual */
   execute() {
     console.log("Do something");
   }
@@ -23,23 +30,9 @@ class MainMenuItem implements FocusableMenuItem {
   static alphaValWhenActive = 1.0;
 }
 
-// Menu
-class DummyContainer {}
-
-class MainMenu extends Menuify<typeof DummyContainer, MainMenuItem>(
-  DummyContainer
-) {
+/** Common MenuBase */
+class BaseObj implements MenuRequiredProps {
   active = false;
-  constructor() {
-    super();
-
-    // Add items
-    for (let i = 0; i < 4; i++) {
-      const item = new MainMenuItem();
-      item.id = i;
-      this.appendItem(item);
-    }
-  }
   activate() {
     this.active = true;
   }
@@ -47,31 +40,63 @@ class MainMenu extends Menuify<typeof DummyContainer, MainMenuItem>(
     this.active = false;
   }
 }
+class MenuCommon extends Menuify<typeof BaseObj, MenuItem>(BaseObj) {}
 
-// Labels
+// Menus
+class MainMenu extends MenuCommon {
+  constructor() {
+    super();
+
+    // Add items
+    for (let i = 0; i < 4; i++) {
+      const item = new MenuItem();
+      item.id = i;
+      this.appendItem(item);
+    }
+  }
+}
+
+class SubMenu extends MenuCommon {
+  constructor() {
+    super();
+
+    for (let i = 0; i < 2; i++) {
+      const item = new MenuItem();
+      item.id = i;
+      this.appendItem(item);
+    }
+  }
+}
+
+// Label-enum
 const MenuEnum = {
   MAIN: "main",
+  SUB: "sub",
 } as const;
 type MenuLabel = typeof MenuEnum[keyof typeof MenuEnum];
 
-describe("MenuController#setActiveMenu", () => {
+/** Custom menu controller */
+class MyMenuController extends MenuController<MenuLabel, MenuCommon> {
+  constructor() {
+    super();
+    this.addMenu(MenuEnum.MAIN, new MainMenu());
+    this.addMenu(MenuEnum.SUB, new SubMenu());
+    this.setActiveMenu("main");
+
+    // Implement process to  main item No.1
+    this.getMenu("main")!.items[0].execute = () => this.setActiveMenu("sub");
+  }
+}
+
+// Test
+describe("MenuController#active-menu", () => {
   test("Should default item of currentMenu be active", () => {
-    const mc = new MenuController<MenuLabel, MainMenu>();
-
-    // Setup MainMenu
-    mc.addMenu(MenuEnum.MAIN, new MainMenu());
-    mc.setActiveMenu("main");
-
-    const currenMenu = mc.currentMenu!;
-    expect(currenMenu.currentItem.isActive()).toBeTruthy();
+    const mc = new MyMenuController();
+    expect(mc.currentMenu!.currentItem.isActive()).toBeTruthy();
   });
 
   test("Should currentMenu be reset by resetState option", () => {
-    const mc = new MenuController<MenuLabel, MainMenu>();
-
-    // Setup mainMenu
-    mc.addMenu(MenuEnum.MAIN, new MainMenu());
-    mc.setActiveMenu("main");
+    const mc = new MyMenuController();
 
     // Change state
     mc.selectNextOption();
@@ -79,6 +104,14 @@ describe("MenuController#setActiveMenu", () => {
     // Reset menu by resetState option
     mc.setActiveMenu("main", true);
 
+    // The 1st item(id:0) should be set as default
     expect(mc.currentMenu!.currentItem.id).toBe(0);
+  });
+
+  test("Should currentMenu switched to subMenu when mainMenu-item-No.1 is executed", () => {
+    const mc = new MyMenuController();
+    mc.setActiveMenu("main", true);
+    mc.runOption();
+    expect(mc.currentMenu instanceof SubMenu).toBeTruthy();
   });
 });
